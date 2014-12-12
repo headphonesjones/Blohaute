07c1bc29e9750f2a280111f3eaa7613c46abbbd6
asporinsky09 <asporinskydev@gmail.com>
2014-12-11 19:49:06 -0600

    Booker Request retries - AJ Sporinsky

--- booking/service.py
+++ booking/service.py
@@ -3,9 +3,23 @@ import requests
 from django.conf import settings
 
 
+class Client(object):
+    base_url = None
+
+    def get(self, path, params):
+        request_url = "%s%s" % (self.base_url, path)
+        response = requests.get(request_url, params=params)
+        return response.json()
+
+    def post(self, path, params):
+        headers = {'content-type': 'application/json'}
+        request_url = "%s%s" % (self.base_url, path)
+        response = requests.post(request_url, data=json.dumps(params), headers=headers)
+        return response.json()
+
+
 class BookerClient(object):
     token = None
-    base_url = None
 
     def __init__(self):
         self.load_token()
@@ -13,18 +27,34 @@ class BookerClient(object):
     def load_token(self):
         params = {'client_id': settings.BOOKER_API_KEY,
                   'client_secret': settings.BOOKER_API_SECRET,
-                  'grant_type': 'client_credentials'
-                  }
+                  'grant_type': 'client_credentials'}
+        response = super(BookerClient, self).get('/access_token', params=params)
+        self.token = response['access_token']
 
-        request = requests.get("%s/access_token" % self.base_url, params=params)
-        self.token = request.json()['access_token']
+    def get(self, path, params):
+        response = self.get(path, params)
+        error_code = response['ErrorCode']
+        if error_code == 1000:
+            self.load_token()
+            response = super(BookerClient, self).get(path, params)
+        if error_code != 0:
+            print("Request to %s with params %s Failed with ErrorCode %s: %s" %
+                  (path, params, error_code, response['ErrorMessage']))
+            # Nathan, should I raise something here? or return some different response?
+            # Not super sure how to do error handling so Im just printing for now
+        return response
 
     def post(self, path, params):
         params['access_token'] = self.token
-        headers = {'content-type': 'application/json'}
-        request_url = "%s%s" % (self.base_url, path)
-        request = requests.post(request_url, data=json.dumps(params), headers=headers)
-        return request.json()
+        response = super(BookerClient, self).post(path, params)
+        error_code = response['ErrorCode']
+        if error_code == 1000:
+            self.load_token()
+            response = super(BookerClient, self).post(path, params)
+        if error_code != 0:
+            print("Request to %s with params %s Failed with ErrorCode %s: %s" %
+                  (path, params, error_code, response['ErrorMessage']))
+        return response
 
 
 class BookerCustomerClient(BookerClient):
@@ -32,22 +62,18 @@ class BookerCustomerClient(BookerClient):
     location_id = None
 
     def get_services(self):
-        params = {}
-        return self.post('/treatments', params)
+        return self.post('/treatments', {})
 
     def get_packages(self):
-        params = {}
-        return self.post('/series', params)
+        return self.post('/series', {})
 
     def get_availability(self, treatment_id, start_date, end_date):
         actual_product = {'IsPackage': False,
-                          'Treatments': {'TreatmentID': treatment_id}
-                          }
+                          'Treatments': {'TreatmentID': treatment_id}}
 
         params = {'StartDateTime': start_date.strftime('%Y%m%d'),
                   'EndDateTime': end_date.strftime('%Y%m%d'),
-                  'Itineraries': actual_product
-                  }
+                  'Itineraries': actual_product}
 
         return self.post('/availability/multiservice', params)
 
@@ -60,5 +86,4 @@ class BookerMerchantClient(BookerClient):
     base_url = 'https://stable-app.secure-booker.com/webservice4/json/BusinessService.svc'
 
     def get_locations(self):
-        params = {}
-        return self.post('/locations', params)
+        return self.post('/locations', {})

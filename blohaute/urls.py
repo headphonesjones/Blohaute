b174c89ecfdc05296544a5154f5412f1b16d4f51
Nathan Brizendine <nathanb@allocone.com>
2014-12-14 22:14:48 +0700

    service page proposal

--- blohaute/urls.py
+++ blohaute/urls.py
@@ -18,17 +18,17 @@ urlpatterns = patterns(
     
 
     url(r'^contact/', TemplateView.as_view(template_name="contact.html"), name='contact'),
-    url(r'^services/', TemplateView.as_view(template_name="services.html"), name='services'),
     url(r'^stylists/', TemplateView.as_view(template_name="stylists.html"), name='stylists'),
     url(r'^partners/', TemplateView.as_view(template_name="partners.html"), name='partners'),
     
     url(r'^cart/', include('changuito.urls')),
-    url(r'^book/', TreatmentList.as_view(template_name="book.html"), name='book'),
     url(r'^checkout/', TemplateView.as_view(template_name="checkout.html"), name='checkout'),
     url(r'^password_lost/', TemplateView.as_view(template_name="password.html"), name='password'),
+
+    url(r'^services/', TreatmentList.as_view(template_name="services.html"), name='services'),
     url(r'^(?P<slug>\w+)/$', TreatmentDetail.as_view(), name='treatment_detail'),
 
-
+    url(r'^$', TemplateView.as_view(template_name="index.html"), name='book'),
     #url(r'^password_change/', TemplateView.as_view(template_name="passwordchange.html"), name='passwordchange'),
 
 

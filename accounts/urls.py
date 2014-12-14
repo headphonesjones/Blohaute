3661b34ac1990a8a232e761fdcf011dfc42a2e88
headphonesjones <josamjohn@gmail.com>
2014-12-13 22:16:11 -0600

    Reschedule page displaying and fixed up

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.views import logout
 from accounts import views
+from django.views.generic.base import TemplateView
 
 urlpatterns = patterns(
     '',
@@ -9,5 +10,5 @@ urlpatterns = patterns(
     url(r'^login/', views.login, name='login'),
     url(r'^welcome/', views.profile_view, name='welcome'),
     url(r'^delete/', views.UserDelete.as_view(), name='delete_user'),
-    url(r'^reschedule/', views.reschedule, name='reschedule'),
+    url(r'^reschedule/', TemplateView.as_view(template_name="registration/reschedule.html"), name='reschedule'),
 )
